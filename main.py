748a50eddfbef2ab97f3d6c27733b7e7d9f143d6
Kelvin Ng <hoishing@gmail.com>
2024-12-21 11:21:45 +0800

    add asteroid

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import requests
 import streamlit as st
+import swisseph as swe
+from datetime import datetime
 from platformdirs import user_cache_path
+from zoneinfo import ZoneInfo
+
+dt = datetime.now()
+local_tz = ZoneInfo("Asia/Taipei")
+local_dt = dt.replace(tzinfo=local_tz)
+utc_dt = local_dt.astimezone(ZoneInfo("UTC"))
+julian_day = swe.date_conversion(
+    utc_dt.year, utc_dt.month, utc_dt.day, utc_dt.hour + utc_dt.minute / 60
+)[1]
+
+bodies = [
+    swe.SUN,
+    swe.MOON,
+    swe.MERCURY,
+    swe.VENUS,
+    swe.MARS,
+    swe.JUPITER,
+    swe.SATURN,
+    swe.URANUS,
+    swe.NEPTUNE,
+    swe.PLUTO,
+    swe.MEAN_NODE,
+    swe.CHIRON,
+    swe.PHOLUS,
+    swe.CERES,
+    swe.PALLAS,
+    swe.JUNO,
+    swe.VESTA,
+]
 
 # Get cache directory for the app
 cache_dir = user_cache_path("swe")
@@ -22,5 +53,15 @@ def download_file():
 
 # Execute download
 file_path = download_file()
+
+swe.set_ephe_path(str(file_path.parent))
+
+for body in bodies:
+    try:
+        ((lon, _, _, speed, *_), _) = swe.calc_ut(julian_day, body)
+        st.write(body, lon, speed)
+    except Exception as e:
+        st.write(body, e)
+
 
 st.write(f"File downloaded to: {file_path}")
